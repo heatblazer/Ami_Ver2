46aea4dc23de61b9bf301506f1561f80e0f79c40
ilianzapryanov <ilian.zapryanov@balkantel.net>
2016-05-25 16:16:58 +0300

    amiclient: nothing fancy, just some text formating

--- amiclient.cpp
+++ amiclient.cpp
@@ -1,7 +1,15 @@
 #include "amiclient.h"
 
+// the default purity implementation
 
-AmiClient::AmiClient(){}
+AmiClient::AmiClient()
+{
 
-AmiClient::~AmiClient() { }
+}
+
+
+AmiClient::~AmiClient()
+{
+
+}
 
